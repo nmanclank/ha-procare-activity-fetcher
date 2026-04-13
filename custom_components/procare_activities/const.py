22a45be6608b9ab5af588ebc69d45d999a6d3284
Ray Whiteside <ray.s.whiteside@gmail.com>
2026-04-12 21:25:38 -0500

    feat: make update interval configurable (closes #9)

--- custom_components/procare_activities/const.py
+++ custom_components/procare_activities/const.py
@@ -4,6 +4,8 @@ DOMAIN = "procare_activities"
 PLATFORMS = ["sensor"]
 
 CONF_SCHOOL_NAME = "school_name"
+CONF_UPDATE_INTERVAL = "update_interval"
+DEFAULT_UPDATE_INTERVAL = 15  # minutes
 
 DEFAULT_AUTH_HOST = "https://online-auth.procareconnect.com"
 DEFAULT_API_HOST = "https://api-school.procareconnect.com"
